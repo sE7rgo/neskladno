c86bca8313b4d4bbdd46ae5e85b2b03d1fd9b746
sergii gorbatyi <se7rgo@gmail.com>
2023-03-01 14:44:45 -0800

    starting routes'

---
+++ src/routes/welcome/Welcome.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Welcome() {
+  return (
+    <div className="Welcome">
+        <p>
+         Welcome
+        </p>
+    
+    </div>
+  );
+}
+
+export default Welcome;
