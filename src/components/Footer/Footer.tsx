8d2e06bbee8518689d09f4d6a5fe82914636026e
sergii gorbatyi <se7rgo@gmail.com>
2024-02-22 18:39:54 -0800

    creating ThemeSwitch components, using it in Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import ThemeSwitch from "../ThemeSwitch";
+
+
+function Footer() {
+    return (
+        <div className='flex flex-col items-center h-2 justify-end'>
+            <ThemeSwitch />
+        </div>
+    );
+  }
+  
+  export default Footer;
+  
