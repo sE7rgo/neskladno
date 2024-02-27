c917d1ab7688579d0187520baffcd10b172b59b3
sergii gorbatyi <se7rgo@gmail.com>
2024-02-27 03:00:00 -0800

    speach recognitio usage

--- src/routes/main/Main.tsx
+++ src/routes/main/Main.tsx
@@ -1,13 +1,72 @@
-import React from "react";
-import { oneLetter } from "../../assets/ua";
-import { useTimer } from "../../hooks/useTimer";
+import React, { useEffect, useState } from "react";
+import { fourLetters } from "../../assets/ua";
+import { useTimer } from "../../utils/hooks/useTimer";
+
+// import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill';
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import { getRandomInt } from "../../utils/getRandomIntr";
+
+// const appId = '<INSERT_SPEECHLY_APP_ID_HERE>';
+// const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
+// SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
 
 function Main() {
+  const [word, setWord] = useState<string | null>(fourLetters[0]);
   const { hours, minutes, seconds } = useTimer();
-  const length = oneLetter.length;
+  const length = fourLetters.length;
 
+  const {
+    transcript,
+    listening,
+    browserSupportsSpeechRecognition,
+    resetTranscript,
+    interimTranscript
+  } = useSpeechRecognition();
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: false, language: "uk" });
+
+  useEffect(() => {
+    if (!transcript.length) return;
+    const formattedTranscription = transcript.toLowerCase();
+    console.log("wdwdwd", "чкск".localeCompare("Чкск"));
+    if (word?.localeCompare(formattedTranscription) === 0) {
+      console.log("correct", word, transcript, word?.toLowerCase === transcript.toLowerCase);
+    } else {
+      console.log(
+        "not correct",
+        word,
+        transcript,
+        word?.localeCompare(formattedTranscription) === 0
+      );
+    }
+    // // TODO: check for int already used
+    const index = getRandomInt(1, length);
+    setWord(fourLetters[index].toLowerCase());
+  }, [transcript]);
+
+  useEffect(() => {
+    console.log(transcript.length);
+  }, [transcript]);
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
   return (
     <div className="flex h-full w-2/4 flex-col items-center justify-center">
+      <p>{word}</p>
+
+      <div>
+        <p>Microphone: {listening ? "on" : "off"}</p>
+        <button
+          onTouchStart={startListening}
+          onMouseDown={startListening}
+          onTouchEnd={SpeechRecognition.stopListening}
+          onMouseUp={SpeechRecognition.stopListening}
+        >
+          Hold to talk
+        </button>
+        <p>{transcript}</p>
+      </div>
       <p>{`${hours}h ${minutes}m ${seconds}s`}</p>
     </div>
   );
--- src/routes/welcome/locales/ua.ts
+++ src/routes/welcome/locales/ua.ts
@@ -1,7 +1,14 @@
 export default {
   welcomePlaceholder:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non lacinia leo, venenatis porttitor augue. Donec aliquet ipsum volutpat, tincidunt leo ac, faucibus turpis. Pellentesque posuere tortor quis lectus feugiat convallis in in eros. Cras varius cursus nisi quis convallis. Ut non dapibus tortor. Aliquam feugiat ornare risus.",
-  increaseTimerMessage: "venenatis porttitor augue",
   increaseDurationMessage: "consectetur adipiscing elit",
-  insertLinkMessage: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  increaseDifficultyMessage: "venenatis porttitor augue",
+  insertLinkMessage: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  placeholderLinkInput: "some placeholder",
+  submitButton: "SUBMIT",
+  basic: "basic",
+  basicPlus: "basic +",
+  avarage: "avarage",
+  hard: "hard",
+  veryHard: "very hard"
 };
